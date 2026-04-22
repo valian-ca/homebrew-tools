0d3954337fc185d4f3c320e1eb595eb29baaaea5
François-Pierre Bouchard <fp@valian.ca>
2026-04-21 22:28:35 -0400

    chore(Formula): bump frn to 0.2.1

--- Formula/frn.rb
+++ Formula/frn.rb
@@ -1,8 +1,8 @@
 class Frn < Formula
   desc "Fast Flutter run launcher with device picker"
   homepage "https://github.com/valian-ca/homebrew-tools"
-  url "https://github.com/valian-ca/homebrew-tools/archive/refs/tags/frn-0.2.0.tar.gz"
-  sha256 "43b5a2603d71017f25997a6fabde14ece393074ffab73f429f0dea18c59637fc"
+  url "https://github.com/valian-ca/homebrew-tools/archive/refs/tags/frn-0.2.1.tar.gz"
+  sha256 "40d01a7c341d8339c5c7987606736bbb56ac371146f1b433f3f6c8e386c3dd60"
   license "MIT"
 
   depends_on "gum"
