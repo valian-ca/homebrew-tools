dcef39afa2d81f5d83a1bcaf02ec7a353933ec43
François-Pierre Bouchard <fp@valian.ca>
2026-06-02 10:33:46 -0400

    chore(Casks): release review-triage 0.4.0
    Bump the review-triage cask to 0.4.0 with the matching release
    artifact sha256.

--- Casks/review-triage.rb
+++ Casks/review-triage.rb
@@ -1,6 +1,6 @@
 cask "review-triage" do
-  version "0.3.1"
-  sha256 "05d3290774f9eb335d8912e909f9a6008ec692d294eb1ddaf5f529ec05e5af41"
+  version "0.4.0"
+  sha256 "6182e65247352cf4bdec808ad6e1fd709321fbe27fa8207c89a9150579cd8a1a"
 
   url "https://github.com/valian-ca/homebrew-tools/releases/download/review-triage-app-#{version}/ReviewTriage-#{version}.zip"
   name "Review Triage"
