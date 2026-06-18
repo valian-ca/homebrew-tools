a1ec636fae470bf4e5f8e634c7231c51fbcac444
François-Pierre Bouchard <fp@valian.ca>
2026-06-18 12:23:27 -0400

    chore(Formula): fill in sha256 for atelierd 0.10.1

--- Formula/atelierd.rb
+++ Formula/atelierd.rb
@@ -2,7 +2,7 @@ class Atelierd < Formula
   desc "Atelier dashboard daemon - local bridge to the cloud event stream"
   homepage "https://github.com/valian-ca/homebrew-tools"
   url "https://github.com/valian-ca/homebrew-tools/archive/refs/tags/atelierd-0.10.1.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "84c05c8aa7d58b98d50a1029812698c6613da7b3e02df5b8aae988b58eb37c83"
   license "MIT"
 
   depends_on "go" => :build
